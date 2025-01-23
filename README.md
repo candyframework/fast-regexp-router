10078564efd80e362954c3500282fc2033953c86
Leo <afuafu@bitlightlabs.com>
2025-01-23 09:42:55 +0800

    change js

--- README.md
+++ README.md
@@ -1,35 +1,20 @@
-## 快速正则路由
+## Fast Router
 
-在现阶段的前端项目中，我们大量使用了前端路由，而这些路由中的参数都是使用正则表达式匹配出来的，随着项目的增大，一个用户请求可能需要
-N 次正则匹配才能够找到与之对应的处理程序
+## Principle
 
-本库就是为了解决这个匹配问题，我们将用户的所有路由进行预先合并，使其成为一个大路由，这样一个请求只需要进行一次匹配即可，大大减少了正则开销。
-
-```
-// 假设我们有如下几个路由
+```typescript
 route1 = '/'
 route2 = '/user/{uid}'
 route3 = '/posts/{id}'
 
-如果一个请求 为 `/posts/1` 那么需要三次才能找到正确匹配
+// The system will process these routes into the following format
+(?:\\/)|(?:\\/user\\/(\\w+))|(?:\\/posts\\/(\\w+))
 ```
 
-FastRouter 的处理过程
+## Usage
 
-```
-route1 = '/'
-route2 = '/user/{uid}'
-route3 = '/posts/{id}'
-
-// 首先系统会将全部路由进行合并，结果类似如下格式，然后作为一个整体匹配一次即可得到结果
-
-(?:\\/)|(?:\\/home\\/(\\w+))|(?:\\/posts\\/(\\w+))
-```
-
-## 使用
-
-```
-const FastRouter = require('../index');
+```typescript
+import FastRouter from 'fast-regexp-router'
 
 const routesList = [
     {
@@ -39,9 +24,6 @@ const routesList = [
         }
     },
     {
-        // 路由使用 大括号 识别参数
-        // 而且可以限定参数类型 比如 '/user/{uid:\\d+}' 可以限制 uid 是数字
-        // 如果不限定参数 那么默认参数可以为 字母 数字 或者 下划线
         route: '/user/{uid}',
         handler: (params) => {
             console.log('user requested, uid is: ' + params.uid);
@@ -55,26 +37,15 @@ const routesList = [
     }
 ];
 
-const reg = new FastRouter.default();
-// 设置路由
+const reg = new FastRouter();
 // reg.setRoutes(routesList);
-// 或者依次设置路由
+// Or
 for(let v of routesList) {
     reg.setRoute(v);
 }
 
-// 进行路由匹配
-// 在进行路由匹配的时候 系统会先将用户预先准备的路由进行合并
-// 这样可以大大缩短正则匹配次数
 const match = reg.exec('/user/123');
 if(match) {
     match.handler(match.parameters);
-}
-
-// 也可以使用 execInOrder 方法进行逐个匹配
-// 这样系统就不会对路由进行合并
-const match2 = reg.execInOrder('/posts/10');
-if(match2) {
-    match2.handler(match2.parameters);
 }
 ```
--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
+export type RouteParameters = Record<string, string> | null;
 export type Route = {
-    route: string
-    handler: any
-}
-
+    route: string;
+    handler: any;
+};
 export type RegExpRouter = {
-    pattern: string
-    parameters: string[] | null
-}
+    pattern: string;
+    parameters: string[] | null;
+};
 
 /**
  * FastRouter
@@ -16,57 +16,57 @@ export default class FastRouter {
 
     public combinedRoutePattern: string = '';
     public combinedRouteParameters: (string[] | null)[] | null = null;
-    
-    protected trimChar(str: string, character: string): string {
-        if(character === str.charAt(0)) {
+
+    public trimChar(str: string, character: string): string {
+        if (character === str.charAt(0)) {
             str = str.substring(1);
         }
-        if(character === str.charAt(str.length - 1)) {
+        if (str.length > 0 && character === str.charAt(str.length - 1)) {
             str = str.substring(0, str.length - 1);
         }
 
         return str;
     }
 
-    protected setRoutes(routeList: Route[]): void {
+    public setRoutes(routeList: Route[]): void {
         this.routeList = routeList;
     }
 
-    protected setRoute(route: Route): void {
+    public setRoute(route: Route): void {
         this.routeList.push(route);
     }
 
     /**
      * Parse route
-     * 
-     * ```
+     *
+     * ```text
      * pattern: /home/{uid}         -> \\/home\\/(\\w+)
      * pattern: /home/{uid}/{page}  -> \\/home\\/(\\w+)\\/(\\w+)
      * pattern: /home/{uid:\\d+}    -> \\/home\\/(\\d+)
      * pattern: /home/profile       -> \\/home\\/profile
      * ```
      */
-    protected parseRoute(patternString: string): RegExpRouter {
+    public parseRoute(patternString: string): RegExpRouter {
         let parameters = null;
 
         // format /home/(uid)/(page:\\d+)
         let pattern = patternString.replace(/\{/g, '(').replace(/\}/g, ')');
 
         // search parameters [ '(uid', '(page:' ]
-        let matchedParams = pattern.match(/\(\w+:?/g);
+        const matchedParams = pattern.match(/\(\w+:?/g);
 
         // replace parameters
-        if(null !== matchedParams) {
+        if (null !== matchedParams) {
             parameters = [];
 
-            for(let i=0,len=matchedParams.length; i<len; i++) {
+            for (let i = 0, len = matchedParams.length; i < len; i++) {
                 // () or (\\d+)
                 pattern = pattern.replace(matchedParams[i], '(');
                 pattern = pattern.replace('()', '(\\w+)');
 
                 matchedParams[i] = matchedParams[i].replace(':', '');
 
-                parameters.push( matchedParams[i].substring(1) );
+                parameters.push(matchedParams[i].substring(1));
             }
         }
 
@@ -75,18 +75,18 @@ export default class FastRouter {
 
         return {
             pattern: pattern,
-            parameters: parameters
+            parameters: parameters,
         };
     }
 
-    protected combineRoutes(): void {
-        let patterns = [];
-        let parameters = [];
+    public combineRoutes(): void {
+        const patterns = [];
+        const parameters = [];
 
-        for(let regExp=null, i=0, len=this.routeList.length; i<len; i++) {
+        for (let regExp = null, i = 0, len = this.routeList.length; i < len; i++) {
             regExp = this.parseRoute(this.routeList[i].route);
 
-            patterns.push( '(?:' + regExp.pattern + ')' );
+            patterns.push('(?:' + regExp.pattern + ')');
             parameters.push(regExp.parameters);
         }
 
@@ -94,12 +94,12 @@ export default class FastRouter {
         this.combinedRouteParameters = parameters;
     }
 
-    protected getSubPatternPosition(matches:RegExpExecArray): number {
+    private getSubPatternPosition(matches: RegExpExecArray): number {
         let position = -1;
 
         // matches: [ '/path/123', undefined, '/path/123', 123]
-        for(let i=1,len=matches.length; i<len; i++) {
-            if(undefined !== matches[i]) {
+        for (let i = 1, len = matches.length; i < len; i++) {
+            if (undefined !== matches[i]) {
                 position = i;
                 break;
             }
@@ -108,12 +108,12 @@ export default class FastRouter {
         return position;
     }
 
-    protected getMatchedRouteIndexByPath(path: string): number {
+    private getMatchedRouteIndexByPath(path: string): number {
         let index = 0;
 
-        let str = this.trimChar(path, '/');
-        for(let i=0, len=this.routeList.length; i<len; i++) {
-            if( str === this.trimChar(this.routeList[i].route, '/') ) {
+        const str = this.trimChar(path, '/');
+        for (let i = 0, len = this.routeList.length; i < len; i++) {
+            if (str === this.trimChar(this.routeList[i].route, '/')) {
                 index = i;
                 break;
             }
@@ -122,25 +122,25 @@ export default class FastRouter {
         return index;
     }
 
-    protected getMatchedRouteIndexBySubPattern(subPatternPosition: number): number {
+    private getMatchedRouteIndexBySubPattern(subPatternPosition: number): number {
         let find = 0;
         let str = '';
-        let pattern = this.combinedRoutePattern;
+        const pattern = this.combinedRoutePattern;
 
-        for(let i=0, len=pattern.length - 1; i<len; i++) {
-            if('(' === pattern[i] && '?' !== pattern[i + 1]) {
+        for (let i = 0, len = pattern.length - 1; i < len; i++) {
+            if ('(' === pattern[i] && '?' !== pattern[i + 1]) {
                 find += 1;
             }
 
-            if(find === subPatternPosition) {
+            if (find === subPatternPosition) {
                 str = pattern.substring(0, i);
                 break;
             }
         }
 
         find = 0;
-        for(let i=0, len=str.length; i<len; i++) {
-            if('|' === str[i]) {
+        for (let i = 0, len = str.length; i < len; i++) {
+            if ('|' === str[i]) {
                 find += 1;
             }
         }
@@ -149,35 +149,35 @@ export default class FastRouter {
     }
 
     public execInOrder(route: string): {
-        handler: any;
-        parameters: Record<string, any> | null;
+        handler: Route['handler'];
+        parameters: RouteParameters;
     } | null {
         let ret = null;
-        let matches: RegExpExecArray | null = null
+        let matches: RegExpExecArray | null = null;
 
-        for(let i=0, regExp=null; i<this.routeList.length; i++) {
+        for (let i = 0, regExp = null; i < this.routeList.length; i++) {
             regExp = this.parseRoute(this.routeList[i].route);
             matches = new RegExp(regExp.pattern).exec(route);
 
             // not match
-            if(null === matches) {
+            if (null === matches) {
                 continue;
             }
 
-            let parameters: Record<string, any> | null = null;
-            if(null !== regExp.parameters) {
+            let parameters: RouteParameters = null;
+            if (null !== regExp.parameters) {
                 parameters = {};
 
-                for(let x=1; x<matches.length; x++) {
-                    if(undefined !== matches[x]) {
-                        parameters[ regExp.parameters[x - 1] ] = matches[x];
+                for (let x = 1; x < matches.length; x++) {
+                    if (undefined !== matches[x]) {
+                        parameters[regExp.parameters[x - 1]] = matches[x];
                     }
                 }
             }
 
             ret = {
                 handler: this.routeList[i].handler,
-                parameters: parameters
+                parameters: parameters,
             };
 
             break;
@@ -187,40 +187,39 @@ export default class FastRouter {
     }
 
     public exec(route: string): {
-        handler: any;
-        parameters: Record<string, any> | null;
+        handler: Route['handler'];
+        parameters: RouteParameters;
     } | null {
         this.combineRoutes();
-        if(null === this.combinedRouteParameters) {
+        if (null === this.combinedRouteParameters) {
             return null;
         }
 
-        let matches = new RegExp(this.combinedRoutePattern).exec(route);
-        
+        const matches = new RegExp(this.combinedRoutePattern).exec(route);
+
         // not match
-        if(null === matches) {
+        if (null === matches) {
             return null;
         }
 
-        let subPatternPosition = this.getSubPatternPosition(matches);
-        let routeIndex = -1 === subPatternPosition
+        const subPatternPosition = this.getSubPatternPosition(matches);
+        const routeIndex = -1 === subPatternPosition
             ? this.getMatchedRouteIndexByPath(matches.input)
             : this.getMatchedRouteIndexBySubPattern(subPatternPosition);
 
-        let parameters: Record<string, any> | null = null;
-        let parameterNames = this.combinedRouteParameters[routeIndex];
-        if(null !== parameterNames) {
+        const parameterNames = this.combinedRouteParameters[routeIndex];
+        let parameters: RouteParameters = null;
+        if (null !== parameterNames) {
             parameters = {};
 
-            for(let i=0,len=parameterNames.length; i<len; i++) {
-                parameters[ parameterNames[i] ] =
-                    matches[subPatternPosition + i];
+            for (let i = 0, len = parameterNames.length; i < len; i++) {
+                parameters[parameterNames[i]] = matches[subPatternPosition + i];
             }
         }
 
         return {
             handler: this.routeList[routeIndex].handler,
-            parameters: parameters
+            parameters: parameters,
         };
     }
 }
